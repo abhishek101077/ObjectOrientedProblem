113ecb0222518ef53594832ad32e450effbac26f
Abhishek shukla <Abhishekgargshu786@gmail.com>
2023-03-30 20:56:28 +0530

    The programme file

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace InventoryManagement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string path = "C:\\Users\\Arindam shukla\\source\\repos\\InventoryManagement\\InventoryManagement";
+            ReadInventory obj = new ReadInventory();  
+            InventoryDetails data = obj.Read(path);
+
+            Console.WriteLine(data.typeOfRice.name);
+            Console.WriteLine(data.typeOfRice.weight);
+            Console.WriteLine(data.typeOfRice.price);
+
+        }
+    }
+}
